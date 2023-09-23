587cc6718fa703444b5654f7755cb18bdb5a2588
Artem Blodov <mr.samy@rambler.ru>
2023-09-24 00:14:38 +0200

    correct func

--- main/files and func/func.py
+++ main/files and func/func.py
@@ -30,10 +30,10 @@ def show_latest_transaction(how, all_=False):
     if all_:
         list_operation = get_data()
     else:
-        list_operation = complit_operation
+        list_operation = complit_operation()
     for i in range(0, how):
         action = Operation(list_operation[i])
-        message = f"""{action.message_date()} {action.message_who()} 
+        message = f"""{action.message_date()} {action.message_who()}
 {action.message_from()} -> {action.message_to()}
 {action.message_how_many()} {action.message_currency()}\n"""
         print(message)
