6e2674cf37c0b2f5f22db905c102a8095d688a3e
Artem Blodov <mr.samy@rambler.ru>
2023-09-24 00:27:50 +0200

    rework start programm

--- main/main.py
+++ main/main.py
@@ -1,9 +1,5 @@
 from files_and_func.func import *  # show_latest_transaction
 
 how = int(input("Сколько последний операций хотите посмотреть? "))
-full = input("Хотите посмотреть только завершенные операции? (Да/Нет) ").lower()
-if full == "да":
-    yep = True
-else:
-    yep = False
-show_latest_transaction(how, yep)
+
+show_latest_transaction(how)
