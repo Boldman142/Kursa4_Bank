d2b4029c997ee08c257a6f9df9ab3712225714a1
Artem Blodov <mr.samy@rambler.ru>
2023-09-24 12:06:32 +0200

    add __name__ == "__main__"

--- base/main.py
+++ base/main.py
@@ -1,5 +1,5 @@
 from base.files_and_func.func import show_latest_transaction
 
-how = int(input("Сколько последних операций хотите посмотреть? "))
-
-show_latest_transaction(how)
+if __name__ == "__main__":
+    how = int(input("Сколько последних операций хотите посмотреть? "))
+    show_latest_transaction(how)
