764f74fb0e18710b72de395d49f295dd0c6ad272
Artem Blodov <mr.samy@rambler.ru>
2023-10-09 15:28:34 +0200

    All good

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -3,8 +3,5 @@ import os
 
 
 def test_load_data():
-    path = os.path.join("operations_2.json")
+    path = os.path.join("tests", "operations_2.json")
     assert isinstance(load_data(path), list)
-
-    # assert load_data(os.path.join("start_data", "operations.json")) == get_all_operations
-    # get_all_operations
